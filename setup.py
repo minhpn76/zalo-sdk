aab684a75568ba8d1dae5dd9a137f24da3123e62
minhpn <phamngocminh7694@gmail.com>
2019-04-23 10:32:43 +0700

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = 'zalo-python-sdk',
+    packages = find_packages(),
+    version = '1.14',
+    description = 'Python SDK for Zalo API',
+    author = 'Nguyen Thuy Khanh Duy',
+    author_email = 'duyntk@vng.com.vn',
+    url = 'https://github.com/ntkduy123/zalo-python-sdk',
+    download_url = 'https://github.com/ntkduy123/zalo-python-sdk/archive/0.1.tar.gz',
+    keywords = [],
+    classifiers = []
+)
